0570ae2c32ecf8cb44e9d2c8f7684576e9e9516f
elias <elias.jm.garcia@gmail.com>
2019-12-20 18:49:51 -0600

    got templating started, as with chapter 2 proper

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,27 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 #[macro_use]
 extern crate rocket;
+#[macro_use]
+extern crate serde_derive;
+
+use serde::Serialize;
+
+use rocket_contrib::templates::Template;
+
+#[derive(Serialize)]
+pub struct TemplateCtx {
+    name: &'static str,
+}
 
 #[get("/")]
-fn index() -> &'static str {
-    "hello, world!"
+fn index() -> Template {
+    let users = ["ghostface killah", "spook", "elias"];
+    Template::render("index", &TemplateCtx { name: users[0] })
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite()
+        .attach(Template::fairing())
+        .mount("/", routes![index])
+        .launch();
 }
