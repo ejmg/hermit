100243fd0771557070ed79c73a4ac3e2a75aa001
elias <elias.jm.garcia@gmail.com>
2019-12-22 15:32:11 -0600

    implemented up through 2.3 of megatutorial

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,41 @@ extern crate serde_derive;
 use rocket_contrib::templates::Template;
 
 #[derive(Serialize)]
-pub struct TemplateContext {
+pub struct Post {
+    body: &'static str,
+}
+
+#[derive(Serialize)]
+pub struct UserContext {
+    title: &'static str,
+    posts: Vec<Post>,
     name: &'static str,
 }
 
 #[get("/")]
 fn index() -> Template {
     let users = ["ghostface killah", "spook", "elias"];
-    Template::render("index", &TemplateContext { name: users[0] })
+    let ghost_face_posts = vec![
+        Post {
+            body: r"Listen, you could never match my velocity
+Too much stamina, glitter in front of cameras
+On the red carpet, still clean your clock like a janitor",
+        },
+        Post {
+            body: r"Got the Montana booth in front of the store
+Made my usual gun check, safety off, come on Frank
+The moment is here, take your fuckin' hood off
+And tell the driver to stay put",
+        },
+    ];
+    Template::render(
+        "index",
+        &UserContext {
+            title: "Home",
+            posts: ghost_face_posts,
+            name: users[0],
+        },
+    )
 }
 
 fn main() {
