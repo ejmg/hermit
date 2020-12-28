b7349481a37029610298b45e2abef6503c89a88d
elias <elias.jm.garcia@gmail.com>
2020-12-28 17:42:26 -0600

    stream

--- migrations/2020-02-01-034404_user/down.sql
+++
@@ -1,3 +0,0 @@
--- -*- mode: sql; sql-product: postgres; -*-
--- This file should undo anything in `up.sql`
-drop table users;
--- migrations/2020-02-01-034404_user/up.sql
+++
@@ -1,6 +0,0 @@
-create table users (
-       id serial primary key,
-       username varchar not null,
-       email varchar not null,
-       password_hash varchar not null
-);
